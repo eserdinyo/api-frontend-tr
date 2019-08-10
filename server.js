fbd5d8cc53d749a4002eed0bf2e463962156f6d4
eserdinyo <muhammeteser29@gmail.com>
2019-08-10 23:28:45 +0300

    fix server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const url = 'https://github.com/frontendistanbul/technologies/blob/master/README.md';
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8081;
 
 app.get('/', (req, res) => {
 
