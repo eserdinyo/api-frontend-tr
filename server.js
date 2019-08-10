4dd19a1d93315c0c126e88ea45d01b52e612d005
eserdinyo <muhammeteser29@gmail.com>
2019-08-10 22:19:36 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+const url = 'https://github.com/frontendistanbul/technologies/blob/master/README.md';
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+
+    axios(url).then(data => {
+        const $ = cheerio.load(data.data);
+        const tableData = $('table > tbody > tr');
+
+        const companies = [];
+
+        tableData.each(function () {
+
+            const name = $(this).find("td:nth-child(1)").text();
+            const city = $(this).find("td:nth-child(2)").text();
+            const www = $(this).find("td:nth-child(3)").text();
+            const tech = $(this).find("td:nth-child(4)").text();
+
+            companies.push({
+                name,
+                city,
+                www,
+                tech,
+            });
+        })
+        return res.send(companies);
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
